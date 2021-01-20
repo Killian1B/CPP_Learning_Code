71a9d98d8ad0366c681fcf71f21989a0da0a9954
Killian Blanchard <kblanc01@etud.u-pem.fr>
2021-01-20 11:52:56 +0100

    Exs first part of chap 2

--- chap-02/1-first_class.cpp
+++ chap-02/1-first_class.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 
+class Person
+{
+private:
+    std::string _name;
+    unsigned int _age = 0u;
+
+public:
+    void set_name(const std::string& name) { _name = name; }
+    void wait(unsigned int age) { _age = age; }
+    const std::string& get_name() { return _name; }
+    unsigned int get_age() { return _age; }
+};
+
 int main()
 {
-    // Person p;
+    Person p;
 
-    // p.set_name("Batman");
-    // p.set_age(23);
+    p.set_name("Batman");
+    p.wait(23);
 
-    // std::cout << "Person named '" << p.get_name() << "' is " << p.get_age() << " years old." << std::endl;
+    std::cout << "Person named '" << p.get_name() << "' is " << p.get_age() << " years old." << std::endl;
 
     return 0;
 }
