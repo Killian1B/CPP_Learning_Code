5434805e36ba3bf2bf9b8f3ed48012a6a36ea961
Killian Blanchard <kblanc01@etud.u-pem.fr>
2021-01-20 12:24:37 +0100

    complet class Rectangle

--- chap-02/4-rectangle/Rectangle.h
+++ chap-02/4-rectangle/Rectangle.h
@@ -1,22 +1,25 @@
 #pragma once
 
+#include <ostream>
+
 class Rectangle
 {
 public:
-    Rectangle(float length, float width)
-        : _length { length }, _width { width }
-    {}
+    Rectangle(float length, float width);
+    Rectangle(float size);
+    Rectangle();
 
     float get_length() const { return _length; }
     float get_width() const { return _width; }
 
-    void scale(float ratio)
-    {
-        _length *= ratio;
-        _width *= ratio;
-    }
+    void scale(float ratio);
+
+    static void set_default_size(float size);
 
 private:
+    static float _default_size;
     float _length;
     float _width;
 };
+
+std::ostream& operator<<(std::ostream& stream, const Rectangle& rect);
