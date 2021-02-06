e5117ed6e7b99d6e0b3bd435809b9e4fcb75078a
Killian Blanchard <kblanc01@etud.u-pem.fr>
2021-02-06 10:37:59 +0100

    Add Chap03 Exercices

--- chap-03/1-vectors.cpp
+++ chap-03/1-vectors.cpp
@@ -1,4 +1,58 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
 int main()
 {
+    //std::vector<int> values { 4, 0 };
+    std::vector<int> values(4, 0);
+    int array[] = { 0, 1, 2, 3, 4, 5 };
+
+    for (const auto v: values)
+    {
+        std::cout << v << std::endl;
+    }
     return 0;
+
+    std::vector<int> part(&array[2], &array[5]);
+    for (const auto v: part)
+    {
+        std::cout << v << std::endl;
+    }
+
+    std::vector<int> full(array, array + 6);
+    for (const auto v: part)
+    {
+        std::cout << v << std::endl;
+    }
+
+    for (size_t left = 0, right = full.size() - 1; left < right; ++left, --right)
+    {
+        std::swap(full[left], full[right]);
+    }
+
+    for (auto it = full.begin(); it != full.end(); it += 2)
+    {
+        it = full.insert(it, *it);
+    }
+
+    while (!full.empty())
+    {
+        full.pop_back();
+    }
+
+    full.erase(full.begin() + 3);
+
+    full.erase(full.begin() + 2, full.begin() + 7);
+
+    const auto it = std::find(full.begin(), full.end(), 5);
+    if (it != full.end())
+    {
+        full.erase(it);
+    }
+
+    std::vector<int> v;
+    v.assign(5, 2);
+    v.assign({ 0, 2, 32, -4, 3 });
+    v.clear();
 }
--- chap-03/3-associatives/main.cpp
+++ chap-03/3-associatives/main.cpp
@@ -1,4 +1,45 @@
+#include <set>
+#include <unordered_set>
+#include <iostream>
+#include <map>
+
+#include "keys.h"
+
 int main()
 {
+    std::set<ComparableDog> dogs_set;
+    ComparableDog medor { "medor", "labrador" };
+    dogs_set.emplace(medor);
+    ComparableDog gus { "gus", "bordercollie" };
+    dogs_set.emplace(gus);
+    ComparableDog medor_clone(medor);
+    dogs_set.emplace(medor_clone);
+    std::cout << dogs_set.size() << std::endl;
+
+    std::unordered_set<HashableDog> dogs_unordered_set;
+    HashableDog medor_hash { "medor", "labrador" };
+    dogs_unordered_set.emplace(medor_hash);
+    HashableDog gus_hash {"gus", "bordercollie"};
+    dogs_unordered_set.emplace(gus_hash);
+    std::cout << dogs_unordered_set.size() << std::endl;
+
+    using Owner = std::pair<std::string, std::string>;
+    std::map<ComparableDog, Owner> owners_by_dog;
+
+    ComparableDog zim { "zim", "poodle" };
+    ComparableDog flippy { "flippy", "spaniel" };
+
+    owners_by_dog.insert(std::make_pair(medor, Owner { "Claire", "David" }));
+    owners_by_dog.insert_or_assign(gus, Owner { "Marc", "Zipstein" });
+    owners_by_dog.emplace(zim, Owner { "Céline", "Noël" });
+    owners_by_dog.try_emplace(flippy, "Vincent", "Nozick");
+
+    owners_by_dog.emplace(gus, Owner { "Claire", "David" });
+    // -> gus is still owned by Marc (emplace does not modify values with existing keys, neither does insert or try_emplace)
+
+    owners_by_dog.insert_or_assign(gus, Owner { "Vincent", "Nozick" });
+    // -> gus is now owned by Vincent (insert_or_assign reassign values with existing keys)
+
+
     return 0;
 }
