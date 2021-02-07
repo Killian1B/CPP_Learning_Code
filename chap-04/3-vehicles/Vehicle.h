dffc6b9c08660fbbfa0c398fd34720704d15ddf5
Killian Blanchard <kblanc01@etud.u-pem.fr>
2021-02-07 18:15:31 +0100

    Chap4-Part3

--- chap-04/3-vehicles/Vehicle.h
+++ chap-04/3-vehicles/Vehicle.h
@@ -4,23 +4,44 @@
 
 class Driver
 {
+private:
+    bool _has_car_licence = false;
+    bool _has_air_licence = false;
+
+public:
+    void pass_car_licence_exam()
+    {
+        _has_car_licence = true;
+    }
+
+    bool has_car_licence() const
+    {
+        return _has_car_licence;
+    }
+   void pass_air_licence_exam()
+    {
+        _has_air_licence = true;
+    }
+
+    bool has_air_licence() const
+    {
+        return _has_air_licence;
+    }
+
 };
 
 class Vehicle
 {
-public:
+protected:
     Vehicle(const Driver& driver)
         : _driver { driver }
     {}
 
+public:
     virtual ~Vehicle() {}
 
-    virtual unsigned int drive() const
-    {
-        std::cerr << "Not implemented" << std::endl;
-        return 0u;
-    }
+    virtual unsigned int drive() const = 0;
 
-private:
+protected:
     const Driver& _driver;
 };
